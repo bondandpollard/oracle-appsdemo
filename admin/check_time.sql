725d25c3f3e3712808849fa08448db4c6827fd50
Ian Bond <134335151+Ian-A-Bond@users.noreply.github.com>
2026-03-29 14:39:42 +0100

    Rename check_time.sql to admin/check_time.sql

---
+++ admin/check_time.sql
@@ -0,0 +1,3 @@
+-- Check the current system date and time
+--
+SELECT to_char(sysdate,'DD/MM/RR HH24:MM:SS') FROM dual;
--- check_time.sql
+++
@@ -1,3 +0,0 @@
--- Check the current system date and time
---
-SELECT to_char(sysdate,'DD/MM/RR HH24:MM:SS') FROM dual;
