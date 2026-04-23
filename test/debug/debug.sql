a4e09c831c9385d4bdd03df3faa3853629db9b0c
BondandPollard <ian.bond@bondpollard.co.uk>
2026-04-23 17:37:54 +0100

    Debug amend heading

--- test/debug/debug.sql
+++ test/debug/debug.sql
@@ -1,7 +1,7 @@
 -- Debugging
 --
 SELECT L.message,
-  to_char(L.logged_at,'DD/MM/YYYY HH24:MI:SS.FF') "HH24:MI:SS.FF not HH24:MM that is hour and monnth!",
+  to_char(L.logged_at,'DD/MM/YYYY HH24:MI:SS.FF') "HH24:MI:SS.FF not HH24:MM that is hour and month!",
   L.user_name,
   L.applog_sqlerrm "SQLERRM",
   L.program_name,
