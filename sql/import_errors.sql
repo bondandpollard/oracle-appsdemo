69aeb9e4796a9cba859b00c275dd916c7cd0296b
BondandPollard <ian.bond@bondpollard.co.uk>
2026-04-23 13:19:40 +0100

    fix titles

--- sql/import_errors.sql
+++ sql/import_errors.sql
@@ -32,8 +32,9 @@ SET PAGESIZE 66
 SET NEWPAGE 0
 SET LINESIZE 200
 
-TTITLE CENTER 'Bond and Pollard Limited' SKIP 1 -
-  CENTER ======================== SKIP 1-
+TITLE SKIP 1 - 
+  CENTER 'Bond and Pollard Limited' SKIP 1 -
+  CENTER  ========================  SKIP 1-
   LEFT 'Data Import Error Report'  -
   RIGHT 'Page:' SQL.PNO SKIP 2
 
