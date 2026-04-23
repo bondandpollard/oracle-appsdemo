bf643791be46c23edecc7ef1a79b15b9c8d78677
BondandPollard <134335151+bondandpollard@users.noreply.github.com>
2026-04-23 17:36:14 +0100

    Fix typo in debug SQL query column alias

--- test/debug/debug.sql
+++ test/debug/debug.sql
@@ -1,7 +1,7 @@
 -- Debugging
 --
 SELECT L.message,
-  to_char(L.logged_at,'DD/MM/YYYY HH24:MI:SS.FF') "DATE TIME DD/MM/YYYY HH24:MI:SS.FF",
+  to_char(L.logged_at,'DD/MM/YYYY HH24:MI:SS.FF') "HH24:MI:SS.FF not HH24:MM that is hour and monnth!",
   L.user_name,
   L.applog_sqlerrm "SQLERRM",
   L.program_name,
