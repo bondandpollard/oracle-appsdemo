da8338fa711ccdbc751335c5d892bd5be0fa2e14
BondandPollard <ian.bond@bondpollard.co.uk>
2026-04-23 13:20:16 +0100

    fix titles

--- test/dates/test_easter_list.sql
+++ test/dates/test_easter_list.sql
@@ -8,8 +8,9 @@ SET PAGESIZE 66
 SET NEWPAGE 0
 SET LINESIZE 132
 
-TTITLE CENTER 'Bond and Pollard Limited' SKIP 1 -
-  CENTER ======================== SKIP 1-
+TITLE SKIP 1 -
+  CENTER 'Bond and Pollard Limited' SKIP 1 -
+  CENTER  ========================  SKIP 1-
   LEFT 'Holidays Based on Easter'  -
   RIGHT 'Page:' SQL.PNO SKIP 2
 
