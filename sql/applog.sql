19a8e4c99af0bc3b16ec0616fb38b080bf4080c0
BondandPollard <ian.bond@bondpollard.co.uk>
2026-04-23 13:05:05 +0100

    Fix title

--- sql/applog.sql
+++ sql/applog.sql
@@ -29,8 +29,9 @@ SET PAGESIZE 66
 SET NEWPAGE 0
 SET LINESIZE 132
 
-TTITLE CENTER 'Bond and Pollard Limited' SKIP 1 -
-  CENTER ======================== SKIP 1-
+TTITLE SKIP 1 -
+  CENTER 'Bond and Pollard Limited' SKIP 1 -
+  CENTER  ========================  SKIP 1-
   LEFT 'Application Log'  -
   RIGHT 'Page:' SQL.PNO SKIP 2
 SELECT L.message,
