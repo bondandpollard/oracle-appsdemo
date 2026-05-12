44e46b1b7761e570fee9ba081246ba536054a489
BondandPollard <ian.bond@bondpollard.co.uk>
2026-05-12 10:36:47 +0100

    Fix template format, add CPP

--- templates/dos script.bat
+++ templates/dos script.bat
@@ -1,4 +1,5 @@
 REM Copyright (c) 2022 Bond & Pollard Ltd. All rights reserved.
+REM This software is free to use and modify at your own risk.
 REM  
 REM NAME: <script name>.bat
 REM
---
+++ templates/template.cpp
@@ -0,0 +1,22 @@
+/*
+** (c) Bond & Pollard Ltd 2022
+** This software is free to use and modify at your own risk.
+** 
+** Module Name   :
+** Description   :
+** 
+**------------------------------------------------------------------------
+** Modification History
+**  
+** Date            Name                 Description
+**------------------------------------------------------------------------
+** DD/MM/YYYY      <your name>          Program created
+**   
+*/
+
+#include <stdio.h>  
+
+int main(void) {
+ 	
+return 0; 
+}  
