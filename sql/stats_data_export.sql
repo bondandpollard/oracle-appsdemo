e604a77cbac697b1fa81b60ebcfde4bc55d1d945
IAB <>
2026-05-21 12:45:00 +0100

    Added open source free to use at risk comment

--- sql/stats_data_export.sql
+++ sql/stats_data_export.sql
@@ -1,5 +1,7 @@
 /*
-** Copyright (c) 2022 Bond & Pollard Ltd. All rights reserved.  
+** Copyright (c) 2022 Bond & Pollard Ltd. All rights reserved.
+** This software is free to use and modify at your own risk.
+**
 ** NAME   : stats_data_export.sql
 **
 ** DESCRIPTION
